d0da137b3c1ab1c34009434bbee95556c03d9edb
Eric Camellini <eric.camellini@gmail.com>
2022-04-13 12:15:05 +0200

    Selecting a day

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ function displayDays() {
     // 2) We customize it (adding a class, text, etc.)
     dayNode.innerText = dayNumber
     dayNode.classList.add('day')
+    dayNode.onclick = selectDay
+    // ^^ We do not need to call the function, it will be called automatically with the event data passed down
+    // dayNode.addEventListener('click', selectDay) // Another way to do the same thing
 
     // 3) We need to add them to the days container i.e., we manipulate the days container
     // 3.1) We need to select the days container - already done before the loop
@@ -26,6 +29,24 @@ function displayDays() {
     // 3.2) We manipulate it -> we attach a child to it
     daysContainer.appendChild(dayNode)
   }
+}
+
+function selectDay(eventData) {
+  // Before making the clicked day selected...
+  // We need to unselect the one that is currently selected
+  let currentlySelectedNode = document.querySelector('.selected-day')
+  // ^ The same as doing document.getElementsByClassName('selected-day')[0]
+  if (currentlySelectedNode !== null) {
+    currentlySelectedNode.classList.remove('selected-day')
+  }
+
+  // We want to add the selected-day class to the day tag we clicked on
+  // 1) Select the day we clicked on
+  let clickedDay = eventData.target // Event target is the tag where the event happened, it works with every event
+  // ^ in this case, we MUST do this, otherwise how to we select the right day?
+
+  // 2) Manipulate it: add the class
+  clickedDay.classList.add('selected-day')
 }
 
 function executeOnLoad() {
